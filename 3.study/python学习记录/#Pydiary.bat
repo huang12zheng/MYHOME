8481264fda4adf6d010ce7695b8247656206bb2f
Gita <805104533@qq.com>
2017-06-15 09:19:21 +0800

    添加Py学习笔记文件夹

---
+++ 3.study/python学习记录/#Pydiary.bat
@@ -0,0 +1,25 @@
+REM date=2017-01-25 周三
+REM time=21:58:44.94
+set day=%date:~0,4%%date:~5,2%%date:~8,2%_
+REM %time:~0,2%%time:~3,2%
+set filename=%day%py日记.txt
+echo off>%filename%
+echo Keep:今日完成了哪些工作:>>%filename%
+echo [任务--子任务(完成)]>>%filename%
+echo 	1.>>%filename%
+echo 今日未完:>>%filename%
+echo 	1.>>%filename%
+echo Problem:遇到了哪些问题:>>%filename%
+echo 	1.暂停区>>%filename%
+echo 	2.完成区>>%filename%
+echo 	3.解决中>>%filename%
+echo Try:准备尝试哪些措施:>>%filename%
+echo [(需求,设计,测试,完成)]>>%filename%
+echo 	1.>>%filename%
+echo Plan:明日任务:>>%filename%
+echo 	1.>>%filename%
+echo revise:今日反思:>>%filename%
+echo 	1.灵感>>%filename%
+echo 	2.>>%filename%
+echo diary:日记:>>%filename%
+echo 	1.>>%filename%
