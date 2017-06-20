b8e4f6041fa5bcf786190ec163f365eb176fcbb9
hzgood <805104533@qq.com>
2017-06-20 22:16:29 +0800

    添加python学习资料
    ### 添加python学习资料：
    1. 安装包
    2. pip附加库

---
+++ 1.tool/23脚本/3.python/pip_install.bat
@@ -0,0 +1,14 @@
+python -m pip install -U pip
+python -m pip install -U numpy
+python -m pip install -U pandas
+python -m pip install -U virtualenv
+python -m pip install -U ipython
+python -m pip install -U jupyter
+python -m pip install -U Latex
+::python -m pip install -U IPython.display
+::python -m pip install -U IPython.external.mathjax
+python -m pip install -U sympy
+python -m pip install -U request
+::python -m pip install -U caffe
+python -m pip install -U jupyter_contrib_nbextensions
+jupyter contrib nbextension install --user
