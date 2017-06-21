457785b5a9a23a830cb353a63ef56b58345d4ef2
Gita <805104533@qq.com>
2017-06-21 17:56:37 +0800

    python库: arrow scipy matplotlib pymongo sklearn pyqt5 Spyder

--- 1.tool/23脚本/3.python/pip_install.bat
+++ 1.tool/23脚本/3.python/pip_install.bat
@@ -1,6 +1,10 @@
 python -m pip install -U pip
 python -m pip install -U numpy
 python -m pip install -U pandas
+python -m pip install -U arrow
+::http://lib.csdn.net/article/python/64679
+python -m pip install -U scipy
+python -m pip install -U matplotlib
 python -m pip install -U virtualenv
 python -m pip install -U ipython
 python -m pip install -U jupyter
@@ -12,3 +16,11 @@ python -m pip install -U request
 ::python -m pip install -U caffe
 python -m pip install -U jupyter_contrib_nbextensions
 jupyter contrib nbextension install --user
+python -m pip install -U pymongo
+::如果不安装MongoDB会出现后面的一个问题  参考这个链接：http://www.cnblogs.com/billyzh/p/5913687.html
+::学习资料  http://lib.csdn.net/article/python/66880
+python -m pip install -U sklearn
+pip install pyqt5
+pip install Spyder
+::http://lib.csdn.net/article/python/66860
+::按照pip的提示，从landinghub.visualstudio.com/visual-cpp-build-tools下载build tools。带安装好后~一切OK！了
